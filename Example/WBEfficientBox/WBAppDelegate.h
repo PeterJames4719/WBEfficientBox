00ad93cadb659a6e58005fc2a4384a1a2d1170b8
兵伍 <bingwu@bingwudeMBP.lan>
2019-03-10 10:38:04 +0800

    Initial commit

---
+++ Example/WBEfficientBox/WBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WBAppDelegate.h
+//  WBEfficientBox
+//
+//  Created by PeterJames4719 on 03/10/2019.
+//  Copyright (c) 2019 PeterJames4719. All rights reserved.
+//
+
+@import UIKit;
+
+@interface WBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/WBEfficientBox/WBViewController.h
@@ -0,0 +1,13 @@
+//
+//  WBViewController.h
+//  WBEfficientBox
+//
+//  Created by PeterJames4719 on 03/10/2019.
+//  Copyright (c) 2019 PeterJames4719. All rights reserved.
+//
+
+@import UIKit;
+
+@interface WBViewController : UIViewController
+
+@end
